014f8991bfbb5ab67c54de3afaa803bebef4c44e
Ilya Novohatskyi <1llyanovohatskyy@gmail.com>
2018-11-26 20:03:34 +0200

    #7 Add methods for getting data

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const RESOWebApiClient = require('./reso_web_api_client')
 
-const client = new RESOWebApiClient('<Service URL>', auth = {})
+const client = new RESOWebApiClient('http://services.odata.org/V4/OData/OData.svc', auth = {})
+
+/* Example of GET requests */
+
+client.get('Products')
+  .then(d => console.log(d.data.value))
+  .catch(err => console.log(`Error: ${err.message}`))
+
+client.find_by('Products', { Name: 'Coffee' })
+  .then(d => console.log(d.data.value))
+  .catch(err => console.log(err))
